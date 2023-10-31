8aa9ce261c2d45d0d3087bf3264c39eea7a367f3
meenakshi <connectmgaur@gmail.com>
2023-10-31 00:41:51 +0000

    "popup.html and pop.js file updatd.timer not disp"

--- background.js
+++ background.js
@@ -1,13 +1,15 @@
-// Track time spent on YouTube.com
-var youtubeTime = 0; // Variable to store the total time spent on YouTube.com
+var youtubeTime = 0;
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete') {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       var activeTab = tabs[0];
       if (activeTab.url.includes('youtube.com')) {
-        youtubeTime += 1; // Increment the time spent by 1 second
+        youtubeTime += 1;
         console.log('Time spent on YouTube: ' + youtubeTime + ' seconds');
+
+        // Send the time data to the popup or sidebar script
+        chrome.runtime.sendMessage({ timeSpent: youtubeTime });
       }
     });
   }
--- popup.js
+++ popup.js
@@ -1,3 +1,36 @@
 window.addEventListener('DOMContentLoaded', function () {
-    // You can perform any necessary initialization here.
-  });
+  var startTime = new Date().getTime(); // Get the current timestamp
+
+  function updateTimer() {
+    var currentTime = new Date().getTime(); // Get the current timestamp
+    var elapsedTime = currentTime - startTime; // Calculate the elapsed time in milliseconds
+
+    // Convert the elapsed time to hours, minutes, and seconds
+    var hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    var minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+
+    // Format the time as HH:MM:SS
+    var formattedTime = padNumber(hours) + ":" + padNumber(minutes) + ":" + padNumber(seconds);
+
+      // Update the timer element with the formatted time
+      var timerElement = document.getElementById("timer");
+      if (timerElement) {
+        timerElement.textContent = formattedTime;
+      }
+
+     // Log the time in the console
+    console.log("Current time:", formattedTime);
+
+    // Request the next frame
+    requestAnimationFrame(updateTimer);
+  }
+
+  // Start the timer
+  updateTimer();
+});
+
+// Helper function to pad a number with leading zeros (e.g., 1 -> 01)
+function padNumber(num) {
+  return num.toString().padStart(2, "0");
+}
