c812a6c4c416b5564b5fa5a5c6354e1055bd17f4
Oliver343 <93383676+Oliver343@users.noreply.github.com>
2023-11-03 14:15:47 +0000

    Sample login added

--- popup.js
+++ popup.js
@@ -1,3 +1,59 @@
+const baseURL = "https://bug-free-dollop-94rw6v5767gfg64-3001.app.github.dev/"
+let loggedInControl = false
+
+function logginToggle (recievedToken) {
+    loggedInControl = !loggedInControl
+    document.getElementById("loginBox").style.display = loggedInControl ? "none" : "inherit"
+    document.getElementById('tokenBox').value = recievedToken
+    localStorage.setItem("token", recievedToken);
+}
+
+function submitButtonFunc() {
+    const email = document.getElementById('emailInput').value;
+    const password = document.getElementById('passwordInput').value;
+    let successCheck = false
+    
+    if (loggedInControl) {
+        logginToggle (" ")
+    } else {
+        fetch( baseURL + "api/token", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({email: email, password: password}),
+        })
+        .then((recieved) => {
+            console.log(recieved.ok)
+            if (recieved.ok) {
+                successCheck = true
+            } else {
+                successCheck = false
+            }
+            return recieved.json()
+        })
+        .then((data) => {
+            if (successCheck) {
+                logginToggle (data["access_token"])
+            } else {
+                document.getElementById('tokenBox').value = data["message"]
+            }
+            console.log(loggedInControl)
+        })
+        .catch((error) => console.log(error))
+    }
+}
+
+// Getting the login button and assigning function to it.
+document.addEventListener('DOMContentLoaded', function() {
+  const loginButton = document.getElementById('loginButton');
+  loginButton.addEventListener('click', function() {
+      submitButtonFunc();
+  });
+});
+
+
+// Olivers code ends here
+
+
 window.addEventListener('DOMContentLoaded', function () {
   var startTime = new Date().getTime(); // Get the current timestamp
 
